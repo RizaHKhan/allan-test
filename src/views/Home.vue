9985aad0dde258fc8fb36e230b6dea7d7c44ce3c
khanr <khanriza@gmail.com>
2021-05-01 09:34:59 -0400

    on mount visit first tab

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,10 +14,18 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
   name: "Home",
+  setup() {
+    const { push } = useRouter();
+
+    onMounted(() => {
+      push("/todos");
+    });
+  },
 });
 </script>
 
