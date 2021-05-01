71c7ec49a6b3de82040d5a0f3cfa16de6fd27aa3
khanr <khanriza@gmail.com>
2021-04-30 23:24:52 -0400

    adjusted test to pass

--- tests/unit/Navbar.spec.ts
+++ tests/unit/Navbar.spec.ts
@@ -2,6 +2,8 @@ import { shallowMount } from "@vue/test-utils";
 import Navbar from "@/components/core/Navbar.vue";
 
 describe("Navbar", () => {
-  const wrapper = shallowMount(Navbar);
-  expect(wrapper.text()).toBe("Vue-App");
+  it("should render Vue-App", () => {
+    const wrapper = shallowMount(Navbar);
+    expect(wrapper.text()).toBe("Vue-App");
+  });
 });
