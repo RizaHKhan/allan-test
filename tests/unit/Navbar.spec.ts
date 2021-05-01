bcd8eb1206d1a336b44fdba57f8bc392c7836c84
khanr <khanriza@gmail.com>
2021-04-30 22:00:38 -0400

    Added test for navbar

---
+++ tests/unit/Navbar.spec.ts
@@ -0,0 +1,8 @@
+import { shallowMount } from "@vue/test-utils";
+import Navbar from "@/components/core/Navbar.vue";
+
+describe("Navbar", () => {
+  const wrapper = shallowMount(Navbar);
+
+  expect(wrapper.text()).toBe("Logo");
+});
