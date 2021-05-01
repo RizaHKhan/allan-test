e37ecb3fe92afd005bbfda54cabf3766a523d880
khanr <khanriza@gmail.com>
2021-04-30 23:23:04 -0400

    added spacing stuff and finished styling navbar

---
+++ src/components/core/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="navbar">
+    <div class="navbar__brand">
+      <img
+        class="navbar__brand--logo"
+        alt="Vue logo"
+        src="../../assets/logo.png"
+      />
+      <h1 class="ml4 navbar__brand--title">Vue-App</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+
+  &__brand {
+    display: flex;
+
+    &--logo {
+      transition: all 0.5s ease;
+      height: 30px;
+      margin: auto;
+    }
+
+    &--title {
+      font-weight: 300;
+      font-size: 1.5rem;
+
+      @include media-down(sm) {
+        display: none;
+      }
+    }
+  }
+
+  &:hover {
+    .navbar__brand--logo {
+      transform: rotateY(360deg);
+    }
+  }
+}
+</style>
